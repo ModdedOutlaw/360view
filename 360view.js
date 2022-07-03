e6e808ddf4396cf0a9684a334dd5087c7eabf141
ModdedOutlaw <moddedoutlaw33@gmail.com>
2022-07-02 21:41:27 -0400

    x

--- 360view.js
+++ 360view.js
@@ -3,6 +3,8 @@ window.addEventListener('load', onVrViewLoad);
 function onVrViewLoad() {
   // Selector '#vrview' finds element with id 'vrview'.
   var vrView = new VRView.Player('#vrview', {
-    image: 'test_pic.png'
+    image: 'londom_portal.png'
+    
   });
 }
+
