809c8779bf0a007bf170fa381388de22a55cef7c
ModdedOutlaw <moddedoutlaw33@gmail.com>
2022-07-04 07:22:11 -0400

    x

--- data.js
+++ data.js
@@ -51,6 +51,36 @@ var APP_DATA = {
         "pitch": -0.6954316158205831,
         "fov": 1.2463883365493036
       },
+      "linkHotspots": [
+        {
+          "yaw": -3.11447716001965,
+          "pitch": 0.2900875257016988,
+          "rotation": 0,
+          "target": "2-grat_portals"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-grat_portals",
+      "name": "grat_portals",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "yaw": 0.04250054125228786,
+        "pitch": 0.07629350383605882,
+        "fov": 1.2463883365493036
+      },
       "linkHotspots": [],
       "infoHotspots": []
     }
